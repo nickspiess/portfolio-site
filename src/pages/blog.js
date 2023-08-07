4d15f98d30a5c49c9b3674dbb2e42fc3e91642d0
nickspiess <nickspiess23@gmail.com>
2023-08-07 09:52:53 -0600

    test

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -14,22 +14,24 @@ const posts = [
 const blog = () => {
 
   return (
-    <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>Ordo Ab Chao</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
-        <div className="lg:col-span-8 col-span-1">
-          <div className={styles.posts}>
-            {posts.map((post) => <PostCard post={post} key={post.title} /> )}
-            </div>
-        </div>
-        <div className="lg:col-span-4 col-span-1">
-            <div className="lg:sticky relative top-8">
-              <PostWidget />
-              <Categories />
-            </div>
+    <div className={styles.blog}>
+      <div className="container mx-auto px-10 mb-8">
+        <Head>
+          <title>Ordo Ab Chao</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+          <div className="lg:col-span-8 col-span-1">
+            <div className={styles.posts}>
+              {posts.map((post) => <PostCard post={post} key={post.title} /> )}
+              </div>
+          </div>
+          <div className="lg:col-span-4 col-span-1">
+              <div className="lg:sticky relative top-8">
+                <PostWidget />
+                <Categories />
+              </div>
+          </div>
         </div>
       </div>
     </div>
