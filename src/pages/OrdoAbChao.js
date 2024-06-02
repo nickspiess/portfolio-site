7a097377198143d287312e4e1a0560a2da486992
nickspiess <nickspiess23@gmail.com>
2024-06-02 14:15:45 -0600

    remove file

--- src/pages/OrdoAbChao.js
+++
@@ -1,9 +0,0 @@
-import React, { useEffect, useState } from 'react';
-import { Layout } from './Layout';
-import 'tailwindcss/tailwind.css';
-import '../styles/globals.scss';
-
-export {default as PostCard } from './PostCard';
-export {default as Categories } from './Categories';
-export {default as PostWidget } from './PostWidget';
-export {default as Header } from './BlogHeader';
