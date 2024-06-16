b4f165392b47f14b339d4213118749e3e420d007
nickspiess <nickspiess23@gmail.com>
2024-06-16 16:25:56 -0600

    notch detection

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,16 +1,35 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import UAParser from 'ua-parser-js';
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-      <link rel="logo" href="favicon.ico" />
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    let ua;
+    if (ctx.req) {
+      const userAgent = ctx.req.headers['user-agent'];
+      ua = UAParser(userAgent);
+    } else {
+      ua = UAParser(); // Default UA parsing for client-side
+    }
+    return { ...initialProps, ua };
+  }
 
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+  render() {
+    const { ua } = this.props;
+    const isMobile = ua.device.type === 'mobile';
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="logo" href="favicon.ico" />
+        </Head>
+        <body className={isMobile ? 'mobile' : 'desktop'}>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
